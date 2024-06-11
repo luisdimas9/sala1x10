c1496100cc7e64946902d29f617f54f3193bc66e
luis dimas <37938811+luisdimas9@users.noreply.github.com>
2024-06-10 21:59:31 -0400

    Update unopordiez.api.js

--- sala/1X10/unopordiez.api.js
+++ sala/1X10/unopordiez.api.js
@@ -1,11 +1,11 @@
 function urlsUnopordiez() {
     return {
-        apiUrl : 'https://7e59-190-205-3319.ngrok-free.app/api/unopordiez',
-        apiUno : 'https://7e59-190-205-3319.ngrok-free.app/api/cargaunopordiez',
-        apiIns : 'https://7e59-190-205-3319.ngrok-free.app/api/institucion',
-        apiObrero : 'https://7e59-190-205-3319.ngrok-free.app/api/funcionary',
-        api : 'https://7e59-190-205-3319.ngrok-free.app/api/funcionarios',
-        apiTotalInt : 'https://7e59-190-205-3319.ngrok-free.app/api/validados',
+        apiUrl : 'https://7e59-190-205-33-19.ngrok-free.app/api/unopordiez',
+        apiUno : 'https://7e59-190-205-33-19.ngrok-free.app/api/cargaunopordiez',
+        apiIns : 'https://7e59-190-205-33-19.ngrok-free.app/api/institucion',
+        apiObrero : 'https://7e59-190-205-33-19.ngrok-free.app/api/funcionary',
+        api : 'https://7e59-190-205-33-19.ngrok-free.app/api/funcionarios',
+        apiTotalInt : 'https://7e59-190-205-33-19.ngrok-free.app/api/validados',
     };
   }
 
